2cedef81ab3c734e103c5cd2b15bae2d4a8e57de
umudo <umudo@umutcan-pc>
2012-11-27 03:53:23 +0200

    üyekontrol ufak değişiklik

--- uyekontrol.php
+++ uyekontrol.php
@@ -1,26 +1,28 @@
 <?php
+
 session_start();
 if (!empty($_SESSION['logged']) && $_SESSION['logged']) {
     if (empty($_SESSION["signature"])) {
         session_unset();
         session_destroy();
+    } else {
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $browser = $_SERVER["HTTP_USER_AGENT"];
+        $sig = $_SESSION["signature"];
+        $salt = substr($sig, 0, 15);
+        $sessionhash = substr($sig, 15, 64);
+        $yenisessionhash = hash("sha256", $ip . $salt . $browser);
+        if ($yenisessionhash !== $sessionhash) {
+            session_unset();
+            session_destroy();
+        }
+        if (!empty($_SESSION["son_islem"]) && (time() - $_SESSION["son_islem"]) > 1800) { //30 dakikadan fazla olduysa.
+            session_unset();
+            session_destroy();
+        }
+        $_SESSION["son_islem"] = time();
+        /* Buraya Daha Fazla Session Sanity Check eklenmeli */
     }
-    $ip = $_SERVER["REMOTE_ADDR"];
-    $browser = $_SERVER["HTTP_USER_AGENT"];
-    $sig = $_SESSION["signature"];
-    $salt = substr($sig, 0, 15);
-    $sessionhash = substr($sig, 15, 64);
-    $yenisessionhash = hash("sha256", $ip . $salt . $browser);
-    if ($yenisessionhash !== $sessionhash) {
-        session_unset();
-        session_destroy();
-    }
-    if (!empty($_SESSION["son_islem"]) && (time() - $_SESSION["son_islem"]) > 1800) { //30 dakikadan fazla olduysa.
-        session_unset();
-        session_destroy();
-    }
-    $_SESSION["son_islem"] = time();
-    /* Buraya Daha Fazla Session Sanity Check eklenmeli */
 }
 $MEMBER_LOGGED = !empty($_SESSION['logged']) && $_SESSION['logged']; //yukardaki işlemlerde session_destroy() olduysa diye en sona eklendi.
 /**
